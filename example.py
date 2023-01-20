0ed9baa88e717540e5030757e0ec8d93f57d658b
DESKTOP-52D22R9\user <da8411@ajou.ac.kr>
2023-01-20 10:12:16 +0900

    주차장 문제

--- example.py
+++ example.py
@@ -1,21 +1,26 @@
-
-import collections
-def solution(id_list, report, k):
+import math
+def solution(fees, records):
     answer = []
-    report = list(set(report))
-    reportHash = collections.defaultdict(set)
-    stoped = collections.defaultdict(int)
+    inTime = [0]*10000
+    isIn = [0]*10000
+    sumT = [0]*10000
+
+    for record in records:
+        a, b, c = record.split()
+        h, m = a.split(":")
+        if c == "IN":
+            inTime[int(b)] = int(h)*60 + int(m)
+            isIn[int(b)] = 1
+        else:
+            sumT[int(b)] += (int(h)*60 + int(m)) - inTime[int(b)]
+            isIn[int(b)] = 0
+            
+    for i in range(10000):
+        if isIn[i] == 1:
+            sumT[i] += (23*60+59) - inTime[i]
     
-    for x in report:
-        a,b = x.split(' ')
-        reportHash[a].add(b)
-        stoped[b] += 1
-        
-    for name in id_list:
-        mail = 0
-        for user in reportHash[name]:
-            if stoped[user] >= k:
-                mail += 1
-        answer.append(mail)
-                
+    for i in range(10000):
+        if sumT[i] > 0:
+            answer.append(fees[1] + max(math.ceil((sumT[i]- fees[0])/fees[2]), 0) * fees[3])
+
     return answer
