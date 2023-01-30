4979fcdd2c984a6fa7a8b90db3ed28590f197993
DESKTOP-52D22R9\user <da8411@ajou.ac.kr>
2023-01-30 20:26:41 +0900

    pro_baekjoon_1181

--- example.py
+++ example.py
@@ -15,3 +15,20 @@ n,k = map(int,input().split())
 scores = list(map(int,input().split()))
 scores.sort(reverse=True) #내림차순 정렬
 print(scores[k-1])
+
+#baekjoon_1181
+
+n = int(input())
+lst = []
+
+for i in range(n):
+    lst.append(input())
+
+set_lst = set(lst)	# set으로 변환해서 중복값을 제거
+lst = list(set_lst)	# 다시 list로 변환
+
+lst.sort()
+lst.sort(key = len)
+
+for i in lst:
+    print(i)
