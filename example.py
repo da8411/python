2ed77459146baeb9992c4573803109a073953a33
DESKTOP-52D22R9\user <da8411@ajou.ac.kr>
2023-01-30 20:19:31 +0900

    pro_baekjoon_25305

--- example.py
+++ example.py
@@ -7,3 +7,10 @@ x.sort() #x정렬
 
 print(int(sum(x)/5)) #평균
 print(x[2]) #중앙값
+
+#baekjoon_25305
+
+n,k = map(int,input().split())
+scores = list(map(int,input().split()))
+scores.sort(reverse=True) #내림차순 정렬
+print(scores[k-1])
