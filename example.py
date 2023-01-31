f27ef464d707ea5abbe82f9bfdbb7fe997e70e6f
DESKTOP-52D22R9\user <da8411@ajou.ac.kr>
2023-01-31 23:43:21 +0900

    pro_baekjoon_10814

--- example.py
+++ example.py
@@ -1,34 +1,12 @@
-#baekjoon_2587
-x = [] 
-
-for i in range(5):
-    x.append(int(input()))
-
-x.sort() #x정렬
-
-print(int(sum(x)/5)) #평균
-print(x[2]) #중앙값
-
-#baekjoon_25305
-
-n,k = map(int,input().split())
-scores = list(map(int,input().split()))
-scores.sort(reverse=True) #내림차순 정렬
-print(scores[k-1])
-
-#baekjoon_1181
-
 n = int(input())
-lst = []
+member_lst = []
 
 for i in range(n):
-    lst.append(input())
+    age, name = map(str, input().split())
+    age = int(age)
+    member_lst.append((age, name))
 
-set_lst = set(lst)	# set으로 변환해서 중복값을 제거
-lst = list(set_lst)	# 다시 list로 변환
+member_lst.sort(key = lambda x : x[0])	## (age, name)에서 age만 비교
 
-lst.sort()
-lst.sort(key = len)
-
-for i in lst:
-    print(i)
+for i in member_lst:
+    print(i[0], i[1])
