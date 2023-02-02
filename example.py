1a018215ce00a13a2923d6f5dec6a2683563e0f9
DESKTOP-52D22R9\user <da8411@ajou.ac.kr>
2023-02-02 23:24:11 +0900

    pro_baekjoon_2798

--- example.py
+++ example.py
@@ -1,21 +1,13 @@
-from collections import Counter
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+l = len(num)
+ans = 0
+for i in range(0, l-2):
+    for j in range(i+1, l-1):
+        for k in range(j+1, l):
+            if(num[i] + num[j] + num[k] > m):
+                continue
+            else:
+                ans = max(ans ,num[i] + num[j] + num[k])
 
-numbers = []
-for _ in range(int(input())):
-    num = int(input())
-    numbers.append(num)
-
-numbers.sort()
-
-cnt = Counter(numbers).most_common(2)
-
-print(round(sum(numbers) / len(numbers)))
-print(numbers[len(numbers) // 2])
-if len(numbers) > 1:
-    if cnt[0][1] == cnt[1][1]:
-        print(cnt[1][0])
-    else:
-        print(cnt[0][0])
-else:
-    print(cnt[0][0])
-print(max(numbers) - min(numbers))
+print(ans)
