c69bdd4028943a772ada9d4b34fe69723620e704
DESKTOP-52D22R9\user <da8411@ajou.ac.kr>
2023-02-22 23:58:59 +0900

    baekjoon_20040

--- example.py
+++ example.py
@@ -1,39 +1,29 @@
-# Root node를 찾아주는 함수
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+parents = [i for i in range(n)]
+endgame = 0
+
 def find(x):
-    if x == parent[x]:
+    if x == parents[x]:
         return x
     else:
-        root_x = find(parent[x])
-        parent[x] = root_x
-        return parent[x]
+        y = find(parents[x])
+        parents[x] = y
+        return y
 
-# y의 Root 노드가 x의 Root 노드와 같지 않으면 
-# y의 Root 노드가 x의 Root 노드의 자식이 되도록 하는 함수
-def union(x,y):
-    root_x = find(x)
-    root_y = find(y)
+def union(x, y, indx):
+    global endgame
+    x = find(x)
+    y = find(y)
+    if x != y:
+        parents[max(x,y)] = min(x,y)
+    elif endgame == 0:
+        endgame = indx
 
-    if root_x!=root_y:
-        parent[root_y] = root_x
-        number[root_x] +=number[root_y]
+for i in range(m):
+    a, b = map(int, input().split())
+    union(a, b, i + 1)
 
-test_cases = int(input())
-
-for _ in range(test_cases):
-    parent = dict()
-    number = dict()
-
-    f = int(input())
-
-    for _ in range(f):
-        x,y = input().split(" ")
-        
-        if x not in parent:
-            parent[x] = x
-            number[x] = 1
-        if y not in parent:
-            parent[y] = y
-            number[y] = 1
-        
-        union (x,y)
-        print(number[find(x)])
+print(endgame)
