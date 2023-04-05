818ab0b2dd2aaba6ade58cf4a69df74fe30f34ab
dajin jeong <da8411@ajou.ac.kr>
2023-04-05 12:14:12 +0900

    baekjoon_15576

--- example.py
+++ example.py
@@ -1,34 +1,19 @@
-n, c = map(int, input().split())
+A, B = map(int, input().split())
 
-array = []
-for i in range(n):
-    array.append(int(input()))
+m = int(input())
 
-array.sort()
+num = list(map(int, input().split()))
 
+result = []
 
-def binary_search(array, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        current = array[0]
-        count = 1
+n = 0
 
-        for i in range(1, len(array)):
-            if array[i] >= current + mid:
-                count += 1
-                current = array[i]
-
-        if count >= c:
-            global answer
-            start = mid + 1
-            answer = mid
-        else:
-            end = mid - 1
-
-
-start = 1
-end = array[-1] - array[0]
-answer = 0
-
-binary_search(array, start, end)
-print(answer)
+for i in range(len(num)):
+    n = n + (num.pop() * (A**i))
+    
+while n:
+    result.append(n%B)
+    n //= B
+    
+while result:
+    print(result.pop(), end=' ')
