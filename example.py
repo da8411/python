de318d486cf763a8c2e541cfd3721ec088d36d6d
dajin jeong <da8411@ajou.ac.kr>
2023-04-13 19:15:49 +0900

    baekjoon_2618

--- example.py
+++ example.py
@@ -1,24 +1,39 @@
-if __name__ == '__main__':
-    A = input()
-    B = input()
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-    # dp[i][j] : A의 i번째까지의 문자열과 B의 j번째까지의 문자열의 LCS
-    dp = [['' for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+# main
+N = int(input()) # 도로 N x N 
+W = int(input()) # 사건 개수
+position = [(1,1),(N,N)]
+for _ in range(W):
+    position.append(tuple(map(int,input().split())))
+dp = [[-1 for _ in range(W+2)] for _ in range(W+2)]
 
-    for i in range(1, len(A) + 1):
-        nowA = A[i-1]
-        for j in range(1, len(B) + 1):
-            nowB = B[j-1]
+def solution(i,j):
+    if i > W or j > W: return 0
+    if dp[i][j] != -1: return dp[i][j]
+    
+    tmp = max(i,j) + 1
+    ni = solution(tmp,j) + abs(position[tmp][0]-position[i][0]) + abs(position[tmp][1]-position[i][1])
+    nj = solution(i,tmp) + abs(position[tmp][0]-position[j][0]) + abs(position[tmp][1]-position[j][1])
+    dp[i][j] = min(ni,nj)
+    return dp[i][j]
 
-            if nowA == nowB:  # 두 문자가 같을 경우
-                dp[i][j] = dp[i-1][j-1] + nowA
-            else:  # 두 문자가 다를 경우
-                if len(dp[i][j-1]) > len(dp[i-1][j]):
-                    dp[i][j] = dp[i][j-1]
-                else:
-                    dp[i][j] = dp[i-1][j]
+def backtracking(x,y):
+    if x > W or y > W: return
+    nc = max(x,y) + 1
+    nx = abs(position[nc][0]-position[x][0]) + abs(position[nc][1]-position[x][1])
+    ny = abs(position[nc][0]-position[y][0]) + abs(position[nc][1]-position[y][1])
 
-    print(len(dp[len(A)][len(B)]))
+    if dp[nc][y]+nx < dp[x][nc]+ny:
+        print(1)
+        backtracking(nc,y)
+    else:
+        print(2)
+        backtracking(x,nc)
+    return
 
-    if len(dp[len(A)][len(B)]) != 0:
-        print(dp[len(A)][len(B)])
+# result
+print(solution(0,1))
+backtracking(0,1)
