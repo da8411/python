798ef8544d73fbd326d1c92d6a5229bb9c0443d0
dajin jeong <da8411@ajou.ac.kr>
2023-04-14 17:56:34 +0900

    baekjoon_13913

--- example.py
+++ example.py
@@ -1,39 +1,29 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+from collections import deque
 
-# main
-N = int(input()) # 도로 N x N 
-W = int(input()) # 사건 개수
-position = [(1,1),(N,N)]
-for _ in range(W):
-    position.append(tuple(map(int,input().split())))
-dp = [[-1 for _ in range(W+2)] for _ in range(W+2)]
+def bfs():
+    q = deque()
+    q.append(n)
+    while q:
+        v = q.popleft()
+        if v == k:
+            print(visited[v])
+            ans = []
+            # path에 저장된 경로 값을 통해 거슬러올라가면서 ans에 저장
+            while v != n:
+                ans.append(v)
+                v = path[v]
+            ans.append(n)
+            ans.reverse()  # 역순으로 저장되어 있으므로 순서를 바꿈
+            print(' '.join(map(str, ans)))
+            return
+        for next_step in (v-1, v+1, v*2):
+            if 0 <= next_step < MAX and not visited[next_step]:
+                visited[next_step] = visited[v] + 1
+                q.append(next_step)
+                path[next_step] = v  # 최종 경로 출력 시 next_step 값을 통해 이전 위치를 알아낼 수 있도록 저장함
 
-def solution(i,j):
-    if i > W or j > W: return 0
-    if dp[i][j] != -1: return dp[i][j]
-    
-    tmp = max(i,j) + 1
-    ni = solution(tmp,j) + abs(position[tmp][0]-position[i][0]) + abs(position[tmp][1]-position[i][1])
-    nj = solution(i,tmp) + abs(position[tmp][0]-position[j][0]) + abs(position[tmp][1]-position[j][1])
-    dp[i][j] = min(ni,nj)
-    return dp[i][j]
-
-def backtracking(x,y):
-    if x > W or y > W: return
-    nc = max(x,y) + 1
-    nx = abs(position[nc][0]-position[x][0]) + abs(position[nc][1]-position[x][1])
-    ny = abs(position[nc][0]-position[y][0]) + abs(position[nc][1]-position[y][1])
-
-    if dp[nc][y]+nx < dp[x][nc]+ny:
-        print(1)
-        backtracking(nc,y)
-    else:
-        print(2)
-        backtracking(x,nc)
-    return
-
-# result
-print(solution(0,1))
-backtracking(0,1)
+n, k = map(int, input().split())
+MAX = 100001
+visited = [0] * MAX
+path = [0] * MAX
+bfs()
