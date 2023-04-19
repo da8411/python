5e6d7c8f1bb0801482cf75fa7e4ab271316ab0c7
dajin jeong <da8411@ajou.ac.kr>
2023-04-19 09:48:08 +0900

    baekjoon_11286

--- example.py
+++ example.py
@@ -1,29 +1,24 @@
-from collections import deque
+import sys, heapq
 
-def bfs():
-    q = deque()
-    q.append(n)
-    while q:
-        v = q.popleft()
-        if v == k:
-            print(visited[v])
-            ans = []
-            # path에 저장된 경로 값을 통해 거슬러올라가면서 ans에 저장
-            while v != n:
-                ans.append(v)
-                v = path[v]
-            ans.append(n)
-            ans.reverse()  # 역순으로 저장되어 있으므로 순서를 바꿈
-            print(' '.join(map(str, ans)))
-            return
-        for next_step in (v-1, v+1, v*2):
-            if 0 <= next_step < MAX and not visited[next_step]:
-                visited[next_step] = visited[v] + 1
-                q.append(next_step)
-                path[next_step] = v  # 최종 경로 출력 시 next_step 값을 통해 이전 위치를 알아낼 수 있도록 저장함
-
-n, k = map(int, input().split())
-MAX = 100001
-visited = [0] * MAX
-path = [0] * MAX
-bfs()
+min_heap = []
+max_heap = []
+n = int(sys.stdin.readline())
+for i in range(n):
+	num = int(sys.stdin.readline())
+	if num > 0: # 양수일 때 최소 힙
+		heapq.heappush(min_heap, num) 
+	elif num < 0: # 음수일 때 최대 힙
+		heapq.heappush(max_heap, -num) 
+	else:
+		if min_heap: # 최소 힙에 값이 존재하고
+			if max_heap: # 맥스 힙에도 값이 존재할 때
+				if min_heap[0] < max_heap[0]: # 최소 힙이 더 작은 경우에는
+					print(heapq.heappop(min_heap)) # 최소 힙 출력
+				else: # 최대 힙이 더 작은 경우에는
+					print(-heapq.heappop(max_heap)) # 최대 힙 * -1 출력
+			else: # 최소 힙만 존재할 때 
+				print(heapq.heappop(min_heap)) # 최소 힙 출력
+		elif max_heap: # 최대 힙만 존재할 때
+			print(-heapq.heappop(max_heap)) # 최대 힙 출력
+		else: # 둘 다 존재하지 않을 때
+			print(0)
