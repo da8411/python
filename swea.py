3df36371c026db213433b32fbed7975e9f04db3c
dajin jeong <da8411@ajou.ac.kr>
2023-05-14 16:34:04 +0900

    swea_1928

--- swea.py
+++ swea.py
@@ -1,22 +1,44 @@
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+decode = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+          'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+          '0','1','2','3','4','5','6','7','8','9','+','/'
+        ]
 for test_case in range(1, T + 1):
-    num = int(input())
-    a=0;b=0;c=0;d=0;e=0
-    while(num !=1):
-        if (num % 11) == 0 :
-            e+=1
-            num = num / 11
-        elif (num % 7) == 0:
-            d+=1
-            num = num / 7
-        elif (num % 5) == 0:
-            c+=1
-            num = num / 5
-        elif (num % 3) == 0:
-            b+=1
-            num = num / 3
-        elif (num % 2) == 0:
-            a+=1
-            num = num / 2
-    print(f"#{test_case} {a} {b} {c} {d} {e}")
+    # 입력받는 글자(인코딩 된 상태)
+    word = list(input())
+ 
+    # 글자의 길이
+    length = len(word)
+ 
+    # word의 글자 -> 표1에 따라 숫자로 변환 -> 이진수로 변환 -> 하나의 res로 만들기
+    res = ''
+ 
+    for q in range(length):
+        # word -> 표1에 따라 숫자로 변환
+        num = decode.index(word[q])
+ 
+        # num -> 이진수로 변환
+        # int형태로 더하면 단순 숫자의 합이 나오므로 str로 변환하고 앞의 0b 제거
+        bin_num = str(bin(num)[2:])
+ 
+        # bin_num의 길이가 6보다 작으면 앞에 0 붙여주기
+        # 1을 이진수로 바꾸면 1로 나옴(필요한 값은 000001임)
+        while len(bin_num) < 6:
+            bin_num = '0' + bin_num
+ 
+        res += bin_num
+    
+    # 문제에서 구하고자 하는 원래 문장
+    r = ''
+ 
+    # 글자의 길이 * 6에서 8비트씩 자름
+    for w in range(length*6//8):
+ 
+        # 8비트씩 자르기
+        # 자른 값을 10진수로 변환
+        e = int(res[w*8:w*8+8],2)
+ 
+        # 아스키코드의 값을 r에 추가
+        r += chr(e)
+ 
+    print('#{} {}'.format(test_case, r))
