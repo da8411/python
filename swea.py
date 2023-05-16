e888a1c5d10516654154b2df3610e0a42d9477c8
dajin jeong <da8411@ajou.ac.kr>
2023-05-16 14:09:23 +0900

    swea_1284

--- swea.py
+++ swea.py
@@ -1,11 +1,17 @@
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    num = int(input())
-    sum = 0
-    for i in range(1,num+1):
-        if (i % 2 == 0):
-            sum -= i
-        elif (i % 2 == 1):
-            sum += i
-    print("#{} {}".format(test_case, sum))
+    num = list(map(int, input().split()))
+    if (num[4] <= num[2]):
+        b_num = num[1]
+    elif num[4] > num[2]:
+        a = num[4] - num[2]
+        b_num = num[1] + num[3]*(a)
+
+    a_num = num[0]*num[4]
+    if (a_num <= b_num):
+        total = a_num
+    elif (a_num > b_num):
+        total = b_num
+        
+    print("#{} {}".format(test_case, total))
