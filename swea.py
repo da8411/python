d51a48e17e93ea23782608d7ece081d60a85765c
dajin jeong <da8411@ajou.ac.kr>
2023-05-17 14:14:57 +0900

    swea_3431

--- swea.py
+++ swea.py
@@ -1,8 +1,13 @@
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    A, B = map(int, input().split())
-    sum = A + B
-    if sum >= 24:
-        sum -= 24
-    print("#{} {}".format(test_case, sum))
+    A, B, C = map(int, input().split())
+    time = 0
+    if C < A:
+        time = A - C
+    elif C >= A | C <= B:
+        time = 0
+    elif C > B:
+        time = -1
+
+    print("#{} {}".format(test_case, time))
