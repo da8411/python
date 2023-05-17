314c810ed7b325547c1e8b606f62117961dcf7a6
dajin jeong <da8411@ajou.ac.kr>
2023-05-17 13:59:50 +0900

    swea_1970

--- swea.py
+++ swea.py
@@ -1,17 +1,15 @@
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    NM = input()
-    a_list = list(map(int, input().split()))
-    b_list = list(map(int, input().split()))
+    num = int(input())
 
-    if len(a_list) > len(b_list) :
-        a_list, b_list = b_list, a_list
+    money = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+    change = [0, 0, 0, 0, 0, 0, 0, 0]
 
-    li = []
-    for k in range(len(b_list)-len(a_list)+1):
-        sum = 0
-        for j in range(len(a_list)):
-            sum += a_list[j]*b_list[j+k]
-        li.append(sum)
-    print("#{} {}".format(test_case, max(li)))
+    for i in range(len(money)) :
+        if num//money[i] != 0 :
+            change[i] = num//money[i]
+            num = num%money[i]
+
+    print("#{}".format(test_case))
+    print(*list)
