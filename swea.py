565bcebd04b01b09d12f70d96d275eca3ebe3816
dajin jeong <da8411@ajou.ac.kr>
2023-05-21 13:14:15 +0900

    swea_5549

--- swea.py
+++ swea.py
@@ -1,14 +1,8 @@
 T = int(input())
 
 for test_case in range(1, T+1):
-    arr = list(map(int, input().split()))
-    sum = 0
-
-    for i in range(0,5):
-        if arr[i] < 40:
-            arr[i] = 40
-        sum += arr[i]
-    avg = sum / len(arr)
-    avg = int(avg)
-
-    print(f"#{test_case} {avg}")
+    N = int(input())
+    if N % 2 == 0:
+        print(f"#{test_case} Even")
+    else:
+        print(f"#{test_case} Odd")
