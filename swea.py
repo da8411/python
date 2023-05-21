6a5901da5c21168468d8a8f185ca6fcff05fb24e
dajin jeong <da8411@ajou.ac.kr>
2023-05-21 13:11:20 +0900

    swea_3314

--- swea.py
+++ swea.py
@@ -1,11 +1,14 @@
-for test_case in range(1, 11):
-    N, N_list= input().split()
-    N = int(N)
-    new_list = []
+T = int(input())
 
-    for i in N_list:
-        if new_list and i == new_list[-1]:
-            new_list.pop()
-        else:
-            new_list.append(i)
-    print("#{} {}".format(test_case, ''.join(new_list)))
+for test_case in range(1, T+1):
+    arr = list(map(int, input().split()))
+    sum = 0
+
+    for i in range(0,5):
+        if arr[i] < 40:
+            arr[i] = 40
+        sum += arr[i]
+    avg = sum / len(arr)
+    avg = int(avg)
+
+    print(f"#{test_case} {avg}")
