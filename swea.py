5bb420c1c5de3d6b20558a5053b954808bdb1676
dajin jeong <da8411@ajou.ac.kr>
2023-05-21 13:29:48 +0900

    swea_4466

--- swea.py
+++ swea.py
@@ -1,8 +1,13 @@
 T = int(input())
 
 for test_case in range(1, T+1):
-    N = int(input())
-    if N % 2 == 0:
-        print(f"#{test_case} Even")
-    else:
-        print(f"#{test_case} Odd")
+    N, K = map(int,input().split())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse =True)
+    sum=0
+
+    for j in range(0,K):
+        sum += arr[j]
+    sum = int(sum)
+
+    print("#{} {}".format(test_case, sum))
